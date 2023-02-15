4fc8278542d99ef50475bb77664e40e02ecc9533
raed-charrad <raed.charrad91@gmail.com>
2023-02-15 14:23:43 +0100

    update

--- src/views/MemoriesPage.vue
+++ src/views/MemoriesPage.vue
@@ -1,5 +1,10 @@
 <template>
     <base-layout pageTitle="All Memories">
+        <template #actions-end>
+            <ion-button router-link="/new-memory" fill="clear" color="primary">
+                <ion-icon slot="icon-only" :icon="add"></ion-icon>
+            </ion-button>
+        </template>
         <memories-list :memories="memories"></memories-list>
     </base-layout>
         
@@ -8,9 +13,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import MemoriesList from '@/components/memories/MemoriesList.vue'
+import { add } from 'ionicons/icons'
+import { IonButton, IonIcon } from '@ionic/vue'
+
 export default defineComponent({
     components: {
-        MemoriesList
+        MemoriesList,
+        IonButton,
+        IonIcon
     },
 
     computed: {
@@ -22,7 +32,7 @@ export default defineComponent({
         
 
         return {
-            
+            add
         }
     }
 })
